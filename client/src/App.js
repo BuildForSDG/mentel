6ce0181dc65732554ff97dfb0ee5368b93191a04
Abubakar Sambo <sadiqasg@gmail.com>
2020-05-21 16:04:46 +0100

    refractor: changed from mdbreact to materialcss

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import LandingPage from "./components/LandingPage";
 import Login from "./components/Login/Login";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import SignUp from "./components/SignUp";
+import Dashboard from "./components/dashboard/Dashboard";
+
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
          <Route path="/" exact component ={LandingPage} />
          <Route path="/login"  component ={Login} />
          <Route path="/signup"  component ={SignUp} />
-
+          <Route path="/dashboard" component={Dashboard} />
+  
       </Switch>  
 
       </BrowserRouter>
