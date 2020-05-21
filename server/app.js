d718235a2f863b1586ea4fba01836e5eced9aaf3
AbonyiXavier <francisabonyi@gmail.com>
2020-05-21 13:10:30 +0100

    ft: forget password and the features added

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ import cors from 'cors';
 import helmet from 'helmet';
 import signUp from './controllers/userSignUp';
 import signIn from './controllers/userSignin';
+import forgetPassword from './controllers/forgetPassword';
+import resetPassword from './controllers/resetPassword';
 
 const app = express();
 
@@ -25,6 +27,8 @@ require('dotenv').config();
 
 app.post('/api/signup', signUp);
 app.post('/api/signin', signIn);
+app.post('/forgetPassword', forgetPassword);
+app.post('/resetpasstoken/:id-:token', resetPassword);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
