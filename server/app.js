347edde051505cf411612dfc6010c96482804fe0
AbonyiXavier <francisabonyi@gmail.com>
2020-05-22 17:48:09 +0100

    make it better

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 require('dotenv').config();
 
 app.post('/api/signup', signUp);
