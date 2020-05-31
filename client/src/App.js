1dab6273a50d208b231e66575973d009b8b5ddc6
Abubakar Sambo <sadiqasg@gmail.com>
2020-05-31 19:09:26 +0100

    feat: added admin dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Login/Login";
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
+import Admin from "./components/admin/Admin";
 
 
 
@@ -12,16 +13,16 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Switch>
-         <Route path="/" exact component ={LandingPage} />
-         <Route path="/login"  component ={Login} />
-         <Route path="/signup"  component ={SignUp} />
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
           <Route path="/dashboard" component={Dashboard} />
-  
-      </Switch>  
+          <Route path="/admin" component={Admin} />
+        </Switch>
 
       </BrowserRouter>
-      
+
     </div>
   );
 }
