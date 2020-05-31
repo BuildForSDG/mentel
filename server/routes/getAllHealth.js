be13863dc6c6f7bf06f08058a47ec0652607f7fa
AbonyiXavier <francisabonyi@gmail.com>
2020-05-31 21:54:12 +0100

    fix: get all professional feature

--- server/routes/getAllHealth.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import getAllHealth from '../controllers/getAllHealth';
-const router = express.Router();
-
-router.get('/gethealth', getAllHealth.getAllHealth);
-
-export default router;
