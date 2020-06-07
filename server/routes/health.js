34175011969c0279b24d94a611e536ce23575195
AbonyiXavier <francisabonyi@gmail.com>
2020-06-07 10:29:02 +0100

    ft: delete healthprofessional features added

--- server/routes/health.js
+++ server/routes/health.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import health from '../controllers/healthprofessionalSignup';
 import getAllHealth from '../controllers/getAllHealth';
+import deleteHealth from '../controllers/deleteHealth';
 import upload from '../middlewares/multer';
 const router = express.Router();
 
 router.post('/register', upload.single('image'), health.register);
 router.get('/gethealth', getAllHealth.getAllHealth);
+router.delete('/health/:id', deleteHealth.deleteSingleHealth);
+router.delete('/health', deleteHealth.deleteHealth);
 
 export default router;
